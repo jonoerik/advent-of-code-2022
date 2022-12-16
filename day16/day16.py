52d7da7804cdeccfb9002097b53e6884ae95d835
Jonathan Franzi <jonoerik@fastmail.fm>
2022-12-16 23:14:53 +1100

    Attempt day16 part2.
    Too much time/memory would be required to run on input file.

--- day16/day16.py
+++ day16/day16.py
@@ -15,9 +15,9 @@ def load(input_path: Path) -> InputData:
     line_regex = re.compile("^Valve (?P<valve>[A-Z]{2}) has flow rate=(?P<flow>[0-9]+); tunnels? leads? to valves? (?P<tunnels>[A-Z]{2}(?:, [A-Z]{2})*)$")
     with open(input_path) as f:
         while line := f.readline():
-            match = line_regex.fullmatch(line.strip())
-            assert match is not None
-            valves[match.group("valve")] = (int(match.group("flow")), {tunnel: 1 for tunnel in match.group("tunnels").split(", ")})
+            re_match = line_regex.fullmatch(line.strip())
+            assert re_match is not None
+            valves[re_match.group("valve")] = (int(re_match.group("flow")), {tunnel: 1 for tunnel in re_match.group("tunnels").split(", ")})
     return valves
 
 
@@ -73,8 +73,59 @@ def part1(input_data: InputData) -> int:
     return released_pressure(input_data, "AA", [], 30)
 
 
+def released_pressure_with_elephant(input_data: InputData,
+                                    my_pos: str,
+                                    my_time_walking: int,
+                                    elephant_pos: str,
+                                    elephant_time_walking: int,
+                                    opened_valves: list[str],
+                                    remaining_time: int,
+                                    memo: dict[tuple[str, int, str, int, frozenset[str], int], int] = {}) -> int:
+    """
+    Same as released_pressure(), but with an elephant helping.
+    time_walking parameters must be 0 for each actor to perform an action. Otherwise the time_walking is decremented.
+    This is so while actors are walking between valves along tunnels of length >= 2, the other actor can still perform actions.
+    Time walking also includes time spent turning a valve.
+    """
+    memo_keys = [(my_pos, my_time_walking, elephant_pos, elephant_time_walking, frozenset(opened_valves), remaining_time),
+                 (elephant_pos, elephant_time_walking, my_pos , my_time_walking, frozenset(opened_valves), remaining_time)]
+    for memo_key in memo_keys:
+        if memo_key in memo:
+            return memo[memo_key]
+
+    if remaining_time <= 0:
+        return 0
+    # Pressures released by taking different actions this turn.
+    options = []
+    match my_time_walking == 0, elephant_time_walking == 0:
+        case True, _:
+            # I move.
+            if my_pos not in opened_valves:
+                t = min(1, elephant_time_walking)
+                options.append(input_data[my_pos][0] * (remaining_time - 1) + released_pressure_with_elephant(input_data, my_pos, 1 - t, elephant_pos, elephant_time_walking - t, opened_valves + [my_pos], remaining_time - t, memo))
+            for dest in input_data[my_pos][1]:
+                t = min(input_data[my_pos][1][dest], elephant_time_walking)
+                options.append(released_pressure_with_elephant(input_data, dest, input_data[my_pos][1][dest] - t, elephant_pos, elephant_time_walking - t, opened_valves, remaining_time - t, memo))
+        case False, True:
+            # Elephant moves.
+            if elephant_pos not in opened_valves:
+                t = min(1, my_time_walking)
+                options.append(input_data[elephant_pos][0] * (remaining_time - 1) + released_pressure_with_elephant(input_data, my_pos, my_time_walking - t, elephant_pos, 1 - t, opened_valves + [elephant_pos], remaining_time - t, memo))
+            for dest in input_data[elephant_pos][1]:
+                t = min(input_data[elephant_pos][1][dest], my_time_walking)
+                options.append(released_pressure_with_elephant(input_data, my_pos, my_time_walking - t, dest, input_data[elephant_pos][1][dest] - t, opened_valves, remaining_time - t, memo))
+        case False, False:
+            assert False
+
+    p = max(options)
+    for memo_key in memo_keys:
+        memo[memo_key] = p
+    return p
+
+
 def part2(input_data: InputData) -> int:
-    pass  # TODO
+    simplify_tunnels(input_data)
+    return released_pressure_with_elephant(input_data, "AA", 0, "AA", 0, [], 26)
 
 
 if __name__ == "__main__":
